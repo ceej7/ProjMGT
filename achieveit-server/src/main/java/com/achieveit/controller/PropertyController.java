09087cff3aca43398c67ba9e2b1caab34adb8531
ceej <ceej_7@hotmail.com>
2020-04-03 18:45:43 +0800

    feat: 准备开发设备资产模块

---
+++ achieveit-server/src/main/java/com/achieveit/controller/PropertyController.java
@@ -0,0 +1,9 @@
+package com.achieveit.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Api(tags = "资产和设备接口", value="以资产和设备为主体的请求")
+public class PropertyController {
+}
