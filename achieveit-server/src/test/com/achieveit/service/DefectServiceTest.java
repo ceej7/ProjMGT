5c73da8d1f890ace40ed804cc01e1580822b4bd5
vickiii7 <745558517@qq.com>
2020-04-06 15:26:00 +0800

    test:DefectService finished

--- achieveit-server/src/test/com/achieveit/service/DefectServiceTest.java
+++ achieveit-server/src/test/com/achieveit/service/DefectServiceTest.java
@@ -84,7 +84,24 @@ public class DefectServiceTest {
     //////////////getFilteredPagedDefectByEid()//////////////
     @Test
     public void happy_path_with_get_filtered_paged_defect_by_eid() throws Exception {
-        Defect defect = new Defect(1, null, null, null, null, null, null, null);
+        Defect defect = new Defect(1, null, null, null, null, "bug", null, null);
+        List<Defect> defects = new ArrayList<>();
+        defects.add(defect);
+
+        when(defectMapper.getByEidCascade(anyInt())).thenReturn(defects);
+        ResponseMsg msg = new ResponseMsg();
+        msg.setStatusAndMessage(404, "请求出现异常");
+        msg = defectService.getFilteredPagedDefectByEid(1, 0, 1,null,"bug");
+        assertEquals(200, msg.getStatus());
+        assertNotNull(msg.getResponseMap());
+        assertNotNull(msg.getResponseMap().get("Defect"));
+        assertNotNull(msg.getResponseMap().get("page_length"));
+        verify(defectMapper).getByEidCascade(anyInt());
+    }
+
+    @Test
+    public void alternate_path_with_get_filtered_paged_defect_by_eid() throws Exception {
+        Defect defect = new Defect(1, null, null, null, null, "bug", null, null);
         List<Defect> defects = new ArrayList<>();
         defects.add(defect);
 
@@ -139,9 +156,304 @@ public class DefectServiceTest {
 
     //////////////deleteDefect()//////////////
     @Test
-    void happypath_with_deleteDefect()throws Exception{
+    void happy_path_with_deleteDefect_ret200()throws Exception{
+        Defect defect = new Defect();
+        EmployeeProject employeeProject = new EmployeeProject();
+        ArrayList<EmployeeProject>  employeeProjects = new ArrayList<EmployeeProject>();
+        employeeProjects.add(employeeProject);
+        when(defectMapper.getByDid(anyInt())).thenReturn(defect);
+        when(employeeProjectMapper.getEmployeeProject(anyString(),anyInt())).thenReturn(employeeProjects);
+        ResponseMsg msg=defectService.deleteDefect(1,1);
+        assertEquals(200, msg.getStatus());
 
+        byte[] a={3};
+        employeeProject.setDefect_authority(a);
+        msg=defectService.deleteDefect(1,1);
+        assertEquals(200, msg.getStatus());
 
+        byte[] b={2};
+        employeeProject.setDefect_authority(b);
+        msg=defectService.deleteDefect(1,1);
+        assertEquals(200, msg.getStatus());
+
+        byte[] c={1};
+        employeeProject.setDefect_authority(c);
+        msg=defectService.deleteDefect(1,1);
+        assertEquals(200, msg.getStatus());
+
+    }
+
+    @Test
+    void alternate_path_with_deleteDefect_ret214()throws Exception{
+        Defect defect = new Defect();
+        EmployeeProject employeeProject = new EmployeeProject();
+        ArrayList<EmployeeProject>  employeeProjects = new ArrayList<EmployeeProject>();
+        employeeProjects.add(employeeProject);
+        when(defectMapper.getByDid(anyInt())).thenReturn(defect);
+        when(employeeProjectMapper.getEmployeeProject(anyString(),anyInt())).thenReturn(employeeProjects);
+        ResponseMsg msg=defectService.deleteDefect(1,1);
+        assertEquals(200, msg.getStatus());
+
+        defect.setEmployee_project_id(1);
+        employeeProject.setEpid(2);
+        msg=defectService.deleteDefect(1,1);
+        assertEquals(214, msg.getStatus());
+
+    }
+
+    @Test
+    void alternate_path_with_deleteDefect_ret212()throws Exception{
+        Defect defect = new Defect();
+        EmployeeProject employeeProject = new EmployeeProject();
+        ArrayList<EmployeeProject>  employeeProjects = new ArrayList<EmployeeProject>();
+        when(defectMapper.getByDid(anyInt())).thenReturn(defect);
+        when(employeeProjectMapper.getEmployeeProject(anyString(),anyInt())).thenReturn(employeeProjects);
+        ResponseMsg msg=defectService.deleteDefect(1,1);
+        assertEquals(212, msg.getStatus());
+    }
+
+    @Test
+    void altenate_path_with_deleteDefect_ret216()throws Exception{
+        Defect defect = new Defect();
+        when(defectMapper.getByDid(anyInt())).thenReturn(null);
+        ResponseMsg msg=defectService.deleteDefect(1,1);
+        assertEquals(216, msg.getStatus());
+    }
+
+    @Test
+    void error_path_with_deleteDefect_ret404()throws Exception{
+        Defect defect = new Defect();
+        when(defectMapper.getByDid(anyInt())).thenThrow(new RuntimeException());
+        ResponseMsg msg=defectService.deleteDefect(1,1);
+        assertEquals(404, msg.getStatus());
+    }
+
+    //////////////addDefect()//////////////
+    @Test
+    void happy_path_with_addDefect_ret200()throws Exception{
+        Defect defect = new Defect();
+        EmployeeProject employeeProject = new EmployeeProject();
+        ArrayList<EmployeeProject>  employeeProjects = new ArrayList<EmployeeProject>();
+        employeeProjects.add(employeeProject);
+        when(employeeProjectMapper.getEmployeeProject(anyString(),anyInt())).thenReturn(employeeProjects);
+        when(defectMapper.add(any())).thenReturn(1);
+        int i=0;
+        while(i<4) {
+            String authoritys[]={"pmAuthority","qaLeaderAuthority","rdLeaderAuthority","else"};
+            String authority=authoritys[i++];
+            ResponseMsg msg = defectService.addDefect(1, "1", authority, "hah", null, null);
+            assertEquals(200, msg.getStatus());
+        }
+    }
+
+    @Test
+    void alternate_path_with_addDefect_ret212()throws Exception{
+        Defect defect = new Defect();
+        EmployeeProject employeeProject = new EmployeeProject();
+        ArrayList<EmployeeProject>  employeeProjects = new ArrayList<EmployeeProject>();
+        employeeProjects.add(employeeProject);
+        when(employeeProjectMapper.getEmployeeProject(anyString(),anyInt())).thenReturn(null);
+        when(defectMapper.add(any())).thenReturn(1);
+        ResponseMsg msg=defectService.addDefect(1,"1","pmAuthority","hah",null,null);
+        assertEquals(212, msg.getStatus());
+    }
+
+    @Test
+    void error_path_with_addDefect_ret404()throws Exception{
+        when(employeeProjectMapper.getEmployeeProject(anyString(),anyInt())).thenThrow(new RuntimeException());
+        ResponseMsg msg=defectService.addDefect(1,"1","pmAuthority","hah",null,null);
+        assertEquals(404, msg.getStatus());
+    }
+
+    //////////////updateDefect()//////////////
+    @Test
+    void happy_path_with_updateDefect_ret200()throws Exception{
+        Defect defect = new Defect();
+        EmployeeProject employeeProject = new EmployeeProject();
+        ArrayList<EmployeeProject>  employeeProjects = new ArrayList<EmployeeProject>();
+        employeeProjects.add(employeeProject);
+        when(employeeProjectMapper.getEmployeeProject(anyString(),anyInt())).thenReturn(employeeProjects);
+        when(defectMapper.getByDid(anyInt())).thenReturn(defect);
+        when(defectMapper.update(any())).thenReturn(1);
+
+        Map<String,String> param=new HashMap<String, String>();
+        param.put("authority_desc","pmAuthority");
+        param.put("desc","haha");
+        param.put("git_repo","git");
+        param.put("commit","commit");
+        param.put("status","bug");
+        ResponseMsg msg=defectService.updateDefect(1,1,param);
+        assertEquals(200, msg.getStatus());
+
+        Map<String,String> param2=new HashMap<String, String>();
+        param2.put("authority_desc","qaLeaderAuthority");
+        param2.put("desc","haha");
+        param2.put("git_repo","git");
+        param2.put("commit","commit");
+        param2.put("status","bug");
+        byte[] a={3};
+        employeeProject.setDefect_authority(a);
+        msg=defectService.updateDefect(1,1,param2);
+        assertEquals(200, msg.getStatus());
+
+        Map<String,String> param3=new HashMap<String, String>();
+        param3.put("authority_desc","rdLeaderAuthority");
+        param3.put("desc","haha");
+        param3.put("git_repo","git");
+        param3.put("commit","commit");
+        param3.put("status","bug");
+        byte[] b={2};
+        employeeProject.setDefect_authority(b);
+        msg=defectService.updateDefect(1,1,param3);
+        assertEquals(200, msg.getStatus());
+
+        Map<String,String> param4=new HashMap<String, String>();
+        param4.put("authority_desc","null");
+        param4.put("desc","haha");
+        param4.put("git_repo","git");
+        param4.put("commit","commit");
+        param4.put("status","bug");
+        byte[] c={1};
+        employeeProject.setDefect_authority(c);
+        msg=defectService.updateDefect(1,1,param4);
+        assertEquals(200, msg.getStatus());
+    }
+
+    @Test
+    void alternate_path_with_updateDefect_ret216()throws Exception{
+        Defect defect = new Defect();
+        EmployeeProject employeeProject = new EmployeeProject();
+        ArrayList<EmployeeProject>  employeeProjects = new ArrayList<EmployeeProject>();
+        employeeProjects.add(employeeProject);
+        when(employeeProjectMapper.getEmployeeProject(anyString(),anyInt())).thenReturn(null);
+        when(defectMapper.getByDid(anyInt())).thenReturn(null);
+        when(defectMapper.update(any())).thenReturn(1);
+
+        Map<String,String> param=new HashMap<String, String>();
+        param.put("authority_desc","pmAuthority");
+        param.put("desc","haha");
+        param.put("git_repo","git");
+        param.put("commit","commit");
+        param.put("status","bug");
+
+        ResponseMsg msg=defectService.updateDefect(1,1,param);
+        assertEquals(216, msg.getStatus());
+    }
+
+    @Test
+    void alternate_path_with_updateDefect_ret212()throws Exception{
+        Defect defect = new Defect();
+        EmployeeProject employeeProject = new EmployeeProject();
+        ArrayList<EmployeeProject>  employeeProjects = new ArrayList<EmployeeProject>();
+        employeeProjects.add(employeeProject);
+        when(employeeProjectMapper.getEmployeeProject(anyString(),anyInt())).thenReturn(null);
+        when(defectMapper.getByDid(anyInt())).thenReturn(defect);
+        when(defectMapper.update(any())).thenReturn(1);
+
+        Map<String,String> param=new HashMap<String, String>();
+        param.put("authority_desc","pmAuthority");
+        param.put("desc","haha");
+        param.put("git_repo","git");
+        param.put("commit","commit");
+        param.put("status","bug");
+
+        ResponseMsg msg=defectService.updateDefect(1,1,param);
+        assertEquals(212, msg.getStatus());
+    }
+
+    @Test
+    void alternate_path_with_updateDefect_ret218()throws Exception{
+        Defect defect = new Defect();
+        EmployeeProject employeeProject = new EmployeeProject();
+        ArrayList<EmployeeProject>  employeeProjects = new ArrayList<EmployeeProject>();
+        employeeProjects.add(employeeProject);
+        when(employeeProjectMapper.getEmployeeProject(anyString(),anyInt())).thenReturn(employeeProjects);
+        when(defectMapper.getByDid(anyInt())).thenReturn(defect);
+        when(defectMapper.update(any())).thenReturn(1);
+
+        Map<String,String> param=new HashMap<String, String>();
+        param.put("authority_desc","pmAuthority");
+        param.put("desc","haha");
+        param.put("git_repo","git");
+        param.put("commit","commit");
+        param.put("status","no");
+
+        ResponseMsg msg=defectService.updateDefect(1,1,param);
+        assertEquals(218, msg.getStatus());
+}
+
+    @Test
+    void alternate_path_with_updateDefect_ret214_01()throws Exception{
+        Defect defect = new Defect();
+        EmployeeProject employeeProject = new EmployeeProject();
+        ArrayList<EmployeeProject>  employeeProjects = new ArrayList<EmployeeProject>();
+        employeeProjects.add(employeeProject);
+        when(employeeProjectMapper.getEmployeeProject(anyString(),anyInt())).thenReturn(employeeProjects);
+        when(defectMapper.getByDid(anyInt())).thenReturn(defect);
+        when(defectMapper.update(any())).thenReturn(0);
+
+        Map<String,String> param=new HashMap<String, String>();
+        param.put("authority_desc","pmAuthority");
+        param.put("desc","haha");
+        param.put("git_repo","git");
+        param.put("commit","commit");
+        param.put("status","bug");
+
+        ResponseMsg msg=defectService.updateDefect(1,1,param);
+        assertEquals(214, msg.getStatus());
+    }
+
+    @Test
+    void alternate_path_with_updateDefect_ret214_02()throws Exception{
+        Defect defect = new Defect();
+        byte[] a={1};
+        byte[] b={2};
+        defect.setAuthority(a);
+        EmployeeProject employeeProject = new EmployeeProject();
+        ArrayList<EmployeeProject>  employeeProjects = new ArrayList<EmployeeProject>();
+        employeeProject.setDefect_authority(b);
+        employeeProjects.add(employeeProject);
+        when(employeeProjectMapper.getEmployeeProject(anyString(),anyInt())).thenReturn(employeeProjects);
+        when(defectMapper.getByDid(anyInt())).thenReturn(defect);
+        when(defectMapper.update(any())).thenReturn(0);
+
+        Map<String,String> param=new HashMap<String, String>();
+        param.put("authority_desc","pmAuthority");
+        param.put("desc","haha");
+        param.put("git_repo","git");
+        param.put("commit","commit");
+        param.put("status","bug");
+
+        defect.setEmployee_project_id(1);
+        employeeProject.setEpid(2);
+
+        ResponseMsg msg=defectService.updateDefect(1,1,param);
+        assertEquals(214, msg.getStatus());
+    }
+
+    @Test
+    void error_path_with_updateDefect_ret404()throws Exception{
+        Defect defect = new Defect();
+        byte[] a={1};
+        byte[] b={2};
+        defect.setAuthority(a);
+        EmployeeProject employeeProject = new EmployeeProject();
+        ArrayList<EmployeeProject>  employeeProjects = new ArrayList<EmployeeProject>();
+        employeeProject.setDefect_authority(b);
+        employeeProjects.add(employeeProject);
+        when(employeeProjectMapper.getEmployeeProject(anyString(),anyInt())).thenThrow(new RuntimeException());
+        when(defectMapper.getByDid(anyInt())).thenReturn(defect);
+        when(defectMapper.update(any())).thenReturn(0);
+
+        Map<String,String> param=new HashMap<String, String>();
+        param.put("authority_desc","pmAuthority");
+        param.put("desc","haha");
+        param.put("git_repo","git");
+        param.put("commit","commit");
+        param.put("status","bug");
+
+        ResponseMsg msg=defectService.updateDefect(1,1,param);
+        assertEquals(404, msg.getStatus());
     }
 
 }
