29c0676619eae4276e07ab0c484741df7e1244ca
Yijun <BenjaminCHEN2016@outlook.com>
2020-04-11 11:21:06 +0800

    .

--- achieveit-client/src/api/activity.js
+++
@@ -1,8 +0,0 @@
-import request from '@/utils/request'
-
-export function getActivities() {
-  return request({
-    url: 'manhour/activity',
-    method: 'get'
-  })
-}
--- achieveit-client/src/api/dashboard.js
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request'
-
-export function fetchDashboard() {
-  return request({
-    url: '/employee/myDashBoard',
-    method: 'get'
-  })
-}
-
